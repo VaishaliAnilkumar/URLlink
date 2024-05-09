f509098cc5ae0f4130f62fe58f62a25fc7c33549
Vaishali Anilkumar <139063713+VaishaliAnilkumar@users.noreply.github.com>
2024-05-09 15:51:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+async function shortenURL() {
+    const url = document.getElementById("url").value;
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+    if (response.ok) {
+        const data = await response.text();
+        document.getElementById('result').innerHTML = `Shortened URL: <a href="${data}" target="_blank">${data}</a>`;
+    } else {
+        document.getElementById('result').innerHTML = "Error shortening URL";
+    }
+}
+
+
+async function longURL() {
+    const url = document.getElementById("url").value;
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${decodeURIComponent(url)}`);
+    if (response.ok) {
+        const longUrl = await response.text();
+        document.getElementById('result').innerHTML = `long URL: <a href="${longUrl}" target="_blank">${longUrl}</a>`;
+    } else {
+        document.getElementById('result').innerHTML = "Error  URL";
+    }
+}
+
+
+
+
